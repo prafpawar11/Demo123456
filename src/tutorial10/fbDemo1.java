a371865b9c62b1161484433d062d6f44ccc3ffda
prafpawar11 <prafpawar11@gmail.com>
2022-08-31 20:41:46 +0530

    aaa

--- src/tutorial10/fbDemo1.java
+++ src/tutorial10/fbDemo1.java
@@ -5,7 +5,6 @@ public class fbDemo1 {
 	
 	public static void main(String[] args) {
 		
-		
 		System.out.println("Facebook clsas Demo");
 		
 		System.out.println("Facebook lastest Code added from other teammate Demo");
